c8bdf2b0e18536b94b96bb7792ce5d4141cec42c
zidan161 <zidanam161@gmail.com>
2022-03-06 13:21:21 +0700

    menambahkan workmanager untuk offline mode

---
+++ app/src/main/java/com/example/efisherypricelist/data/remote/GetApiWorker.kt
@@ -0,0 +1,53 @@
+package com.example.efisherypricelist.data.remote
+
+import android.content.ContentValues
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.efisherypricelist.data.local.MainDatabase
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class GetApiWorker(private val context: Context, params: WorkerParameters): Worker(context, params) {
+
+    private val executorService: ExecutorService = Executors.newSingleThreadExecutor()
+
+    override fun doWork(): Result {
+
+        val db = MainDatabase.getDatabase(context)
+        val api = ApiHelper.getApiService()
+
+        return try {
+            api.getPrices().enqueueData {
+                executorService.execute {
+                    for (i in it) {
+                        db.fishDao().insert(i)
+                    }
+                }
+            }
+            Result.success()
+        } catch(throwable: Throwable) {
+            Result.failure()
+        }
+    }
+
+    private fun <T> Call<T>.enqueueData(callback: (T) -> Unit) {
+        enqueue(object : Callback<T> {
+            override fun onResponse(call: Call<T>, response: Response<T>) {
+                if (response.isSuccessful) {
+                    callback(response.body() as T)
+                } else {
+                    Log.e(ContentValues.TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<T>, t: Throwable) {
+                Log.e(ContentValues.TAG, "onFailure: ${t.message.toString()}")
+            }
+        })
+    }
+}
